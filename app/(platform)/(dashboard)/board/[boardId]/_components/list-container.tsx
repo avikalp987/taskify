75c26837bbc4699c982d7a5d6a2eeac7d6ef9182
vikalp <avikalp233@gmail.com>
2024-03-20 08:07:26 +0530

    List header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -3,6 +3,8 @@
 import { ListWithCards } from "@/types";
 import { List } from "@prisma/client"
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
     data: ListWithCards[];
@@ -13,8 +15,24 @@ export const ListContainer = ({
     data,
     boardId
 }: ListContainerProps) => {
+
+    const [orderedData, setOrderedData] = useState(data)
+
+    useEffect(() => {
+        setOrderedData(data)
+    }, [data])
+
     return (
-        <ol className="">
+        <ol className="flex gap-x-3 h-full">
+            {orderedData.map((list, index)=>{
+                return(
+                    <ListItem 
+                        key={list.id}
+                        index={index}
+                        data={list}
+                    />
+                )
+            })}
             <ListForm />
             <div className="flex-shrink-0 w-1"/>
         </ol>
